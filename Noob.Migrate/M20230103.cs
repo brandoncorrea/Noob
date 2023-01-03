c63e096b727d76d23885e8cacd2a1c8cb4720e4c
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2023-01-03 03:47:21 -0500

    Create migration for GuildCounts table

---
+++ Noob.Migrate/M20230103.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Noob.DL;
+namespace Noob.Migrate;
+
+/// <summary>
+/// Create new GuildCounts table
+/// </summary>
+public class M20230103 : IMigration
+{
+    public void Migrate(NoobDbContext db) =>
+        db.Database.ExecuteSqlRaw(
+            "CREATE TABLE \"GuildCounts\" (" +
+            "    \"GuildId\" INTEGER NOT NULL CONSTRAINT \"PK_GuildCounts\" PRIMARY KEY AUTOINCREMENT," +
+            "    \"ChannelId\" INTEGER NOT NULL," +
+            "    \"LastUserId\" INTEGER NOT NULL," +
+            "    \"Current\" INTEGER NOT NULL," +
+            "    \"Record\" INTEGER NOT NULL);"
+        );
+}
